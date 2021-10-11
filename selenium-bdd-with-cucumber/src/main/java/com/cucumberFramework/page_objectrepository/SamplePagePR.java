270c0eef92fae5d628f9e1555fc0a8dc06115188
Balanew1993 <92271137+Balanew1993@users.noreply.github.com>
2021-10-11 11:21:14 +0530

    commit

--- selenium-bdd-with-cucumber/src/main/java/com/cucumberFramework/page_objectrepository/SamplePagePR.java
+++ selenium-bdd-with-cucumber/src/main/java/com/cucumberFramework/page_objectrepository/SamplePagePR.java
@@ -9,13 +9,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+import java.util.Set;
 
 import org.apache.log4j.Logger;
 import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.jasypt.properties.EncryptableProperties;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import com.cucumberFramework.CommonLibraries.ExcelLib;
@@ -35,20 +39,24 @@ public class SamplePagePR{
 	
 	private static Logger log = LoggerHelper.getLogger(SamplePagePR.class);	
 	
-	@FindBy(xpath="//input[@id='email']")
+	//@FindBy(xpath="//input[contains(@placeholder,'Email')]")
+	@FindBy(xpath="//input[contains(@type,'email')]")
 	private WebElement username1;
 
-	@FindBy(xpath="//input[@id='passwd']")
+	@FindBy(xpath="//span[contains(text(),'Next')]")
+	private WebElement next;
+	
+	@FindBy(xpath="//input[contains(@type,'password')]")
 	private WebElement password1;	
 	
 	@FindBy(xpath="//div[@class='recaptcha-checkbox-checkmark']")
 	private WebElement recaptcha;
 	
-	@FindBy(xpath="//*[@id='SubmitLogin']/span")
+	@FindBy(xpath="//button[@type='submit']")
 	private WebElement login;
 	
-	@FindBy(xpath="//a[@class='logout']")
-	private WebElement logout;	
+	@FindBy(xpath="//span[@id='myAccountTrigger']")
+	private WebElement Account;	
 	
 	public SamplePagePR(WebDriver driver) {
 		this.driver = driver;
@@ -126,4 +134,53 @@ public class SamplePagePR{
 		}		
 		return value;
 	}
+	
+	public boolean Login() throws Throwable
+	{
+		Map<String, String> actual = Value();
+		String url1=actual.get("URL");
+		String username2=actual.get("username");
+		String password2=actual.get("password");
+		boolean value=true;
+		driver.get(url1);
+		driver.manage().window().maximize();		
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+		//wlib.waitForPageToLoad();
+		waitHelper.waitForPageToLoad();			   
+		WebElement Google = driver.findElement(By.xpath("//button[contains(text(),'Google')]"));
+		Google.click();
+		Thread.sleep(4000);
+		String parentWindow = driver.getWindowHandle();
+		Set<String> handles =  driver.getWindowHandles();
+		   for(String windowHandle  : handles)
+		       {
+		       if(!windowHandle.equals(parentWindow))
+		          {
+		          driver.switchTo().window(windowHandle);
+		          //driver.switchTo().window(parentWindow);
+		          }
+		       }
+		   
+		username1.sendKeys(username2);
+		next.click();
+		waitHelper.WaitForElement(password1,60);
+		password1.sendKeys(password2);
+		next.click();
+		
+		//jse.executeScript("arguments[0].click();", login);
+		
+		waitHelper.waitForPageToLoad();
+		if(Account.isDisplayed())
+		{
+			log.info(" Logged in to Application ");
+		}
+		else
+		{
+			value=false;
+			log.info("Failed to Log into the Application");
+		}		
+		return value;
+	}
+
+	
 }
